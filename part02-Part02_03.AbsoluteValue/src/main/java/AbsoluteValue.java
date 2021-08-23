e6e92bf4f5dc093706b2f52e57c42c358b83057f
Rakesh <crakeshm1@gmail.com>
2021-08-23 12:45:52 -0500

    more code

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,9 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = scanner.nextInt();
+
+        int result = n < 0 ? -1 * n : n;
+        System.out.println(result);
     }
 }
--- part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -5,6 +5,13 @@ public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Give a number:");
+            int n = scanner.nextInt();
+            if(n == 4){
+                break;
+            }
+        }
 
     }
 }
