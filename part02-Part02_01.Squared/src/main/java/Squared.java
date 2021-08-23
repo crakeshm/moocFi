de50153fc1d597454133e18af86250403f58e561
Rakesh <crakeshm1@gmail.com>
2021-08-23 11:07:35 -0500

    feat: more code

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,9 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int num = Integer.valueOf(scanner.nextLine());
+        int squrd = num*num;
+        
+        System.out.println(squrd);
     }
 }
