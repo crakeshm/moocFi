9b3ae0d3560f7926b9122affa923e96c95af1b62
Rakesh <crakeshm1@gmail.com>
2021-08-27 23:46:21 -0500

    3rd unit continued

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -4,7 +4,19 @@ import java.util.Scanner;
 public class Login {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter username: ");
+        String usr = x.nextLine();
+        System.out.println("Enter password: ");
+        String pass = x.nextLine();
+        comparePass(usr, pass);
+    }
+    
+    public static void comparePass(String usr, String pass){
+        if( (usr.equals("alex") && pass.equals("sunshine") || (usr.equals("emma") && pass.equals("haskell")))){
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
     }
 }
